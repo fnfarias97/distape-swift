766d9ae3720f731320e3f3b5f3be238cd5610c93
Fernando Farias <fnfarias97@gmail.com>
2021-12-14 15:29:28 -0300

    gestures animations

--- MyApp/Views/WelcomeViewController.swift
+++ MyApp/Views/WelcomeViewController.swift
@@ -24,10 +24,40 @@ class WelcomeViewController: UIViewController {
         imageLogo.image = UIImage(named: "Logo")
         // imagenes del sistema: UIImage(systemName: "person.fill")
         // Do any additional setup after loading the view.
+        gesturesManager()
+    }
+
+    private func gesturesManager() {
+        self.imageLogo.isUserInteractionEnabled = true
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
+        self.imageLogo.addGestureRecognizer(pinch)
+        // let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        // self.imageLogo.addGestureRecognizer(tap)
+
+    }
+
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else { return }
+        guard let compiled = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return }
+        guard let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String else { return }
+        let alert = UIAlertController(title: "Info",
+                                      message: "Copyright @ 2021\n\(appName)\nVersion \(version) (\(compiled))",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Got it", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @objc func handlePinch(_ sender: UIPinchGestureRecognizer) {
+        print("hola")
+        if let scale = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)) {
+                   guard scale.a > 1.0 else { return }
+                   guard scale.d > 1.0 else { return }
+                    sender.view?.transform = scale
+                   sender.scale = 1
+        }
     }
 
     @IBAction func dismiss(_ sender: Any) {
-        // navigationController?.popViewController(animated: true)
         goToLoginController()
     }
 }
